cae57dee07c6d2d440220194b0335f36ed9f89bf
Sofian Dimassi <sofian.dimassi97@gmail.com>
2024-10-02 15:23:26 +0200

    exercice 2.3

--- rappelpython.py
+++ rappelpython.py
@@ -23,3 +23,14 @@ i = 1
 while i <= 5:
     print(i)
     i += 1
+
+# Initialiser la variable pour stocker la somme
+somme = 0
+i = 2  # Commencer à 2, le premier nombre pair
+# Utiliser une boucle while pour parcourir les nombres pairs jusqu'à 100
+while i <= 100:
+    somme += i  # Ajouter le nombre pair à la somme
+    i += 2  # Passer au nombre pair suivant
+print("La somme des nombres pairs entre 1 et 100 est :", somme)
+
+    
